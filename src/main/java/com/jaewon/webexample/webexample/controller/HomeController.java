99ac0a0920596643cf96d8823a508c061321b18f
Jaewon <jwshin77@hanmail.net>
2021-11-09 16:53:16 +0900

    layout 구성

---
+++ src/main/java/com/jaewon/webexample/webexample/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.jaewon.webexample.webexample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping
+    public String index(){
+        return "index";
+    }
+}
